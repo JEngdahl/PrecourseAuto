2f537a39041f7e82aaa6c8500e7e79b4320e110e
Jarett Engdahl <jaretten@gmail.com>
2018-01-06 00:35:24 -0500

    small change to query for init

--- server/client/index.js
+++ server/client/index.js
@@ -7,15 +7,30 @@ $("document").ready(function(){
   "method": "GET",
   "headers": {
     "Content-Type": "application/json",
-    }
+  }
 }
 
 $.ajax(settings).done(function (response) {
   console.log(response);
   var splitResponse = response.split(" ");
   splitResponse.forEach(function(e){
-   $("#classList") 
+   $("#classList").append("<li>"+e+"</li>")
   })
-}); 
+});
  console.log("yas")
+})
+
+$(document).on('click','#listCohorts li',function(){
+  var clicked = this.innerText
+  var settings = {
+   "async": true,
+   "crossDomain": true,
+   "url": "http://34.207.251.58:3000/api/class"+clicked,
+   "method": "GET",
+   "headers": {
+     "Content-Type": "application/json",
+  }
+  $.ajax(settings).done(function(r){
+    console.log(r)
+  })
 })
