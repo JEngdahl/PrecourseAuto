08feb4dfe29e2063b8ad89bcdb13a890aaaca8fc
Jarett Engdahl <jaretten@gmail.com>
2018-01-12 17:51:30 -0500

    view koans repo

--- server/routes.js
+++ server/routes.js
@@ -8,19 +8,19 @@ module.exports = function(app,db,lb) {
   app.get('/repo',function(req,res){
     //http://localhost:3000/repo?class=SSP7&user=cmourani&repo=recursion
     var r = req.query
-    if (r.repo === rc){
+    if (r.repo === "rc"){
       res.sendFile(lb+'/ClassContainer/'+r.class+'/'+r.user+'/recursion/SpecRunner.html')
     }
-    if (r.repo === ko){
+    if (r.repo === "ko"){
       res.sendFile(lb+'/ClassContainer/'+r.class+'/'+r.user+'/javascript-koans/KoansRunner.html')
     }
     // if (r.repo === tw){
     //   res.sendFile(lb+'/ClassContainer/'+r.class+'/'+r.user+'/'+twittler+'/src/test.html')
     // }
-    if (r.repo === tb){
+    if (r.repo === "tb"){
       res.sendFile(lb+'/ClassContainer/'+r.class+'/'+r.user+'/testbuilder/index.html')
     }
-    if (r.repo === ub){
+    if (r.repo === "ub"){
       res.sendFile(lb+'/ClassContainer/'+r.class+'/'+r.user+'/underbar/SpecRunner.html')
     }
   })
