abe77c9fb535940720eeac6e6efe8de6f3ebfef2
Jarett Engdahl <jaretten@gmail.com>
2018-01-12 17:50:11 -0500

    view koans repo

--- server/client/index.js
+++ server/client/index.js
@@ -46,7 +46,7 @@ $(document).on('click','#listCohorts li',function(){
       return 0
     })
     r.forEach(function(el){
-      $("#cContainer").append("<li>"+el.FullName+" <a href='http://34.207.251.58:3000/repo?class="+el.Class+"&user="+el.GithubName+"&repo="+ko+"'>Koans</a>:<div class='"+el.KoansColor+"'>"+el.Koans+"</div> Recursion:<div class='"+el.RecursionColor+"'>"+el.Recursion+"</div> Underbar:<div class='"+el.UnderbarColor+"'>"+el.UnderbarOne+"</div></li>")
+      $("#cContainer").append("<li>"+el.FullName+" <a href='http://34.207.251.58:3000/repo?class="+el.Class+"&user="+el.GithubName+"&repo=ko'>Koans</a>:<div class='"+el.KoansColor+"'>"+el.Koans+"</div> Recursion:<div class='"+el.RecursionColor+"'>"+el.Recursion+"</div> Underbar:<div class='"+el.UnderbarColor+"'>"+el.UnderbarOne+"</div></li>")
     })
   })
 })
