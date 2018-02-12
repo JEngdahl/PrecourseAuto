7f12360f02a1427dade2f055ef82bb1c5c4bfe6a
Jarett Engdahl <jaretten@gmail.com>
2018-02-12 17:55:48 -0500

    working on refactor for grading repos + sh script

--- grading/recursion.js
+++ grading/recursion.js
@@ -34,10 +34,7 @@ casper
 })
 .then(function(){
   var content = "module.exports = " + JSON.stringify(tempData)
-  fs.write('./../newData.js',content,function(err){
-    if (err) throw err;
-    console.log('The file has been saved!');
-  });
+  fs.write('./../newData.js',content)
 })
 
 casper.run();
