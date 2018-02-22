70d36d0af2f9d98c2befa347c01de40d1f02966b
Jarett Engdahl <jaretten@gmail.com>
2018-02-22 15:31:54 -0500

    changed koans from 54 to 27

--- server/routes.js
+++ server/routes.js
@@ -43,7 +43,7 @@ module.exports = function(app,db,compare) {
       }
       res.send(resp.map(function(el){
 
-        el.KoansPercent = Math.round(convertToPercentage(54,el.Koans))
+        el.KoansPercent = Math.round(convertToPercentage(27,el.Koans))
         el.UnderbarPercent = Math.round(convertToPercentage(71,el.UnderbarOne-57))
         el.RecursionPercent = Math.round(convertToPercentage(2,el.Recursion))
         if(el.Testbuilder > 3299){
@@ -114,6 +114,10 @@ module.exports = function(app,db,compare) {
   })
 
   app.get('/:campus/:cohort/:student',function(req,res){
+    res.sendFile(__dirname+'/client/build/index.html')
+  })
+
+  app.get('/*',function(req,res){
     res.sendFile(__dirname+'/client/build/index.html')
   })
 
