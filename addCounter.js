e26065b27b185ef2f068b255ba81ed755839267f
Jarett Engdahl <jaretten@gmail.com>
2018-02-22 21:49:47 -0500

    new lirbary

--- addCounter.js
+++ addCounter.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
-//var path = __dirname + '/test/ssp15-recursion/src/'
-var path = __dirname + '/server/client/ClassContainer/'+process.argv[2]+'/'+process.argv[3]+'/recursion/src/'
+var strip = require('strip-comments');
+var path = __dirname + '/test/ssp15-recursion/src/'
+//var path = __dirname + '/server/client/ClassContainer/'+process.argv[2]+'/'+process.argv[3]+'/recursion/src/'
 
 console.log(path+"getElementsByClassName.js")
 fs.exists(path+"getElementsByClassName.js",function(exists){
@@ -8,12 +9,12 @@ fs.exists(path+"getElementsByClassName.js",function(exists){
   if(exists){
     fs.readFile(path+"getElementsByClassName.js","utf-8", (err, data) => {
       if (err) throw err;
-      var string = data;
-      var match = string.match(/var getElementsByClassName = function\(.*\) {/)
+      var string = strip(data);
+      var match = string.match(/var getElementsByClassName = function.*\(.*\) {/)
       if(!match){
         match = string.match(/var getElementsByClassName = function\(.*\n\) {/)
       }
-      string = string.replace(match[0],"\nvar counter = 0; \n" + match[0] + " \ncounter++ \nif(counter > 2000){\n\treturn\n}")
+      string = string.replace(match[0],"\nvar counter = 0; \n" + match[0] + " \ncounter++ \nif(counter > 2000){\n\treturn\n}","")
       fs.writeFile(path + "getElementsByClassName.js",string,(err, data) => {
         if(err) throw err;
       })
