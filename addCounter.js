aafb809aec496796fab11fe5c88ae8cb71391350
Jarett Engdahl <jaretten@gmail.com>
2018-02-22 21:18:18 -0500

    added case

--- addCounter.js
+++ addCounter.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
-//var path = __dirname + '/test/ssp15-recursion/src/'
-var path = __dirname + '/server/client/ClassContainer/'+process.argv[2]+'/'+process.argv[3]+'/recursion/src/'
+var path = __dirname + '/test/ssp15-recursion/src/'
+//var path = __dirname + '/server/client/ClassContainer/'+process.argv[2]+'/'+process.argv[3]+'/recursion/src/'
 
 console.log(path+"getElementsByClassName.js")
 fs.exists(path+"getElementsByClassName.js",function(exists){
@@ -10,32 +10,35 @@ fs.exists(path+"getElementsByClassName.js",function(exists){
       if (err) throw err;
       var string = data;
       var match = string.match(/var getElementsByClassName = function\(.*\) {/)
+      if(!match){
+        match = string.match(/var getElementsByClassName = function\(.*\n\) {/)
+      }
       string = string.replace(match[0],"\nvar counter = 0; \n" + match[0] + " \ncounter++ \nif(counter > 2000){\n\treturn\n}")
       fs.writeFile(path + "getElementsByClassName.js",string,(err, data) => {
         if(err) throw err;
       })
     });
-    fs.readFile(path+"parseJSON.js","utf-8", (err, data) => {
-      if (err) throw err;
-      var string = data;
-      var match = string.match(/var parseJSON = function\(.*\) {/)
-      string = string.replace(match[0],"\nvar counter = 0; \n" + match[0] + " \ncounter++ \nif(counter > 2000){\n\treturn\n}")
-      fs.writeFile(path + "parseJSON.js",string,(err, data) => {
-        if(err) throw err;
-      })
-    });
-    fs.readFile(path+"stringifyJSON.js","utf-8", (err, data) => {
-      if (err) throw err;
-      var string = data;
-      var match = string.match(/var stringifyJSON = function\(.*\) {/)
-      if(!match){
-        match = string.match(/function stringifyJSON\(.*\) {/)
-      }
-      string = string.replace(match[0],"\nvar counter = 0; \n" + match[0] + " \ncounter++ \nif(counter > 2000){\n\treturn\n}")
-      fs.writeFile(path+"stringifyJSON.js",string,(err, data) => {
-        if(err) throw err;
-      })
-    });
+    // fs.readFile(path+"parseJSON.js","utf-8", (err, data) => {
+    //   if (err) throw err;
+    //   var string = data;
+    //   var match = string.match(/var parseJSON = function\(.*\) {/)
+    //   string = string.replace(match[0],"\nvar counter = 0; \n" + match[0] + " \ncounter++ \nif(counter > 2000){\n\treturn\n}")
+    //   fs.writeFile(path + "parseJSON.js",string,(err, data) => {
+    //     if(err) throw err;
+    //   })
+    // });
+    // fs.readFile(path+"stringifyJSON.js","utf-8", (err, data) => {
+    //   if (err) throw err;
+    //   var string = data;
+    //   var match = string.match(/var stringifyJSON = function\(.*\) {/)
+    //   if(!match){
+    //     match = string.match(/function stringifyJSON\(.*\) {/)
+    //   }
+    //   string = string.replace(match[0],"\nvar counter = 0; \n" + match[0] + " \ncounter++ \nif(counter > 2000){\n\treturn\n}")
+    //   fs.writeFile(path+"stringifyJSON.js",string,(err, data) => {
+    //     if(err) throw err;
+    //   })
+    // });
   }
 })
 console.log(process.argv[2], process.argv[3])
