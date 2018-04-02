9913a8c46326191fc20c8d9843e54939b7084bfe
Henri Pietila <henrismail@gmail.com>
2018-04-02 15:45:16 -0700

    fixes unbar

--- gradeRepos.sh
+++ gradeRepos.sh
@@ -57,10 +57,10 @@ for i in $CLASSLIST; do
   for j in $STUDENTLIST; do
     echo $j
     rm -rf ./server/client/ClassContainer/$i/$j
-    git clone --quiet https://github.com/$j/$i-recursion ./server/client/ClassContainer/$i/$j/recursion
+    #git clone --quiet https://github.com/$j/$i-recursion ./server/client/ClassContainer/$i/$j/recursion
     # git clone --quiet https://github.com/$j/$i-twittler ./server/client/ClassContainer/$i/$j/twittler
-    git clone --quiet https://github.com/$j/$i-testbuilder ./server/client/ClassContainer/$i/$j/testbuilder
-    git clone --quiet https://github.com/$j/$i-javascript-koans ./server/client/ClassContainer/$i/$j/javascript-koans
+    #git clone --quiet https://github.com/$j/$i-testbuilder ./server/client/ClassContainer/$i/$j/testbuilder
+    #git clone --quiet https://github.com/$j/$i-javascript-koans ./server/client/ClassContainer/$i/$j/javascript-koans
     git clone --quiet https://github.com/$j/$i-underbar ./server/client/ClassContainer/$i/$j/underbar
 
     if [ -e ./server/client/ClassContainer/$i/$j/underbar/src/underbar.js ]; then
