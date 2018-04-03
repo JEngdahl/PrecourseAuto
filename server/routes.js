595b023e966d8d9c68cfc9a7d51115b81b61ad09
Jarett Engdahl <jaretten@gmail.com>
2018-04-03 12:39:34 -0400

    req.body

--- server/routes.js
+++ server/routes.js
@@ -197,7 +197,7 @@ module.exports = function(app,db,compare) {
     app.post('/api/updateone', function(req, res){
       var r = req.body;
       console.log(r)
-        db.query("UPDATE precourse.Students SET "+r.repo+" = "+r.score+" WHERE GithubName ="+r.github+";",function(err){
+        db.query("UPDATE precourse.Students SET "+r.repo+" = "+r.score+" WHERE GithubName ='"+r.github+"';",function(err){
           if(err){
             console.log(err)
           } else {
