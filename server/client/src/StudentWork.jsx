3cae7e11a1fdfc13761b3e91fc173e038d345e3d
Jarett Engdahl <jaretten@gmail.com>
2018-04-03 11:04:35 -0400

    New build

--- server/client/src/StudentWork.jsx
+++ server/client/src/StudentWork.jsx
@@ -3,11 +3,11 @@ import React, { Component } from "react";
 class StudentWork extends Component {
   openWork(student){
     console.log("clicked")
-    window.open(`http://34.207.251.58:9000/${student.Class}/${student.GithubName}/javascript-koans/KoansRunner.html`)
-    window.open(`http://34.207.251.58:9000/${student.Class}/${student.GithubName}/testbuilder/index.html`)
-    window.open(`http://34.207.251.58:9000/${student.Class}/${student.GithubName}/recursion/SpecRunner.html`)
-    window.open(`http://34.207.251.58:9000/${student.Class}/${student.GithubName}/underbar/SpecRunner.html`)
-    window.open(`http://34.207.251.58:9000/${student.Class}/${student.GithubName}/twittler/index.html`)
+    window.open(`http://35.173.188.239:9000/${student.Class}/${student.GithubName}/javascript-koans/KoansRunner.html`)
+    window.open(`http://35.173.188.239:9000/${student.Class}/${student.GithubName}/testbuilder/index.html`)
+    window.open(`http://35.173.188.239:9000/${student.Class}/${student.GithubName}/recursion/SpecRunner.html`)
+    window.open(`http://35.173.188.239:9000/${student.Class}/${student.GithubName}/underbar/SpecRunner.html`)
+    window.open(`http://35.173.188.239:9000/${student.Class}/${student.GithubName}/twittler/index.html`)
   }
 
   render(){
