93fc2a69b5934822c53448416fffe37767c6538d
Jarett Engdahl <jaretten@gmail.com>
2018-04-05 14:58:02 -0400

    refactor front end partial changes + build

--- server/routes.js
+++ server/routes.js
@@ -43,14 +43,23 @@ module.exports = function(app,db,compare) {
       }
       res.send(resp.map(function(el){
 
-        el.KoansPercent = Math.round(convertToPercentage(27,el.Koans))
-        el.UnderbarPercent = Math.round(convertToPercentage(71,el.UnderbarOne-57))
+        el.KoansPercent = Math.round(convertToPercentage(54,el.Koans))
+
+        el.UnderbarOnePercent = Math.round(convertToPercentage(62,el.UnderbarOne))
+        el.UnderbarTwoPercent = Math.round(convertToPercentage(58,el.UnderbarTwo))
         el.RecursionPercent = Math.round(convertToPercentage(2,el.Recursion))
+        if(el.RecursionPercent > 100){
+          el.RecursionPercent = 100;
+        }
+
         if(el.Testbuilder > 3299){
           el.TestbuilderPercent = 100;
         } else {
           el.TestbuilderPercent = Math.round(convertToPercentage(3299,el.Testbuilder))
         }
+
+        el.Overall = ((el.KoansPercent || 0) + (el.UnderbarOnePercent || 0) + (el.UnderbarTwoPercent || 0) + (el.TestbuilderPercent || 0) + (el.RecursionPercent || 0)) / 5
+
         return el
       }));
     })
@@ -131,7 +140,7 @@ module.exports = function(app,db,compare) {
       if(err){
         console.log(err)
       }
-      console.log(resp[0])
+      let keys = Object.keys(resp[0])
 
     })
   })
