efe83e5d143353a40d8c6137ed2fd4f6a4d526af
John Jacob <jacobjohnrobert@gmail.com>
2018-05-03 15:28:05 -0500

    remove console.log from Err component

--- server/client/src/Err.jsx
+++ server/client/src/Err.jsx
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 class Err extends Component {
   
   render(){
-    console.log('kikiskfdksdfafsda');
     return (
       <div><h1>¯\_(ツ)_/¯</h1></div>
       
