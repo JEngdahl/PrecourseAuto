456fc51f09bc329608cce9526abbb9b1258aad00
John Jacob <jacobjohnrobert@gmail.com>
2018-05-03 16:50:37 -0500

    add build folder back to repo, change hardcoded URL

--- server/client/src/baseUrl.js
+++ server/client/src/baseUrl.js
@@ -4,7 +4,7 @@ let BASE_URL;
 if (hostname === 'localhost') {
   BASE_URL = 'http://localhost:3000';
 } else {
-  BASE_URL = 'http://35.173.188.239:3000';
+  BASE_URL = 'http://ec2-107-23-198-130.compute-1.amazonaws.com:3000';
 }
 
 export default BASE_URL;
